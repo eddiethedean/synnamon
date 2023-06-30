9fb5641106478cb447e4335846eba2c16ae1a20e
Odos Matthews <odosmatthews@Odoss-MacBook-Pro.local>
2023-06-30 11:40:36 -0400

    version change

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="synnamon",
-    version="0.2.0",
+    version="0.1.0",
     description="Pure Python package for getting synonyms for words.",
     long_description=README,
     long_description_content_type="text/markdown",
--- synnamon/__init__.py
+++ synnamon/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.2.0'
+__version__ = '0.1.0'
 
 from synnamon.get_syns import get_syns
